9ed878a6f21238d925a7be83dbb4978b9a7b9edd
adonis adonis <adonisnowman@gmail.com>
2022-09-30 15:08:15 +0800

    change index.php

--- phalcon/controllers/UploadsController.php
+++ phalcon/controllers/UploadsController.php
@@ -28,15 +28,15 @@ class UploadsController extends BaseController
         $Mp4File = "video/{$id}.mp4";
        
         if(!empty($_FILES) && file_exists($_FILES['file']['tmp_name']) == false) exit;
-            echo $exec = " /opt/local/bin/ffmpeg -i {$_FILES['file']['tmp_name']} -y $Mp4File 2>&1 ";
+            echo $exec = " ffmpeg -i {$_FILES['file']['tmp_name']} -y $Mp4File 2>&1 ";
             if (exec($exec, $out)) {
                 
                 if (file_exists($Mp4File) == false) exit;
                 exec(" sudo chmod 777 {$Mp4File} ", $out);
                 var_dump($out);
-                exec(" /opt/local/bin/ffmpeg -i {$Mp4File} -pix_fmt rgb24 -r 4 video/{$id}.gif 2>&1", $out);
+                exec(" ffmpeg -i {$Mp4File} -pix_fmt rgb24 -r 4 video/{$id}.gif 2>&1", $out);
                 var_dump($out);
-                exec(" /opt/local/bin/ffmpeg -re -i $Mp4File -c copy -f flv  -flvflags no_duration_filesize rtmp://adonis.tw/live/{$id} 2>&1", $out);
+                exec(" ffmpeg -re -i $Mp4File -c copy -f flv  -flvflags no_duration_filesize rtmp://adonis.tw/live/{$id} 2>&1", $out);
                 var_dump($out);
             }
         
