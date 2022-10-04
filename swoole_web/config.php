bc9095dddf3011440777345623531a49a623a73d
adonis adonis <adonisnowman@gmail.com>
2022-10-04 13:05:19 +0800

    change keys

--- swoole_web/config.php
+++ swoole_web/config.php
@@ -1,4 +1,4 @@
 <?php
-$SIGNALING_ADDRESS = 'wss://adonis.tw:9509';
+$SIGNALING_ADDRESS = 'wss://swoole.bestaup.com:9509';
 // $SIGNALING_ADDRESS = 'wss://adonis.tw:8080';
 //$STUN_ADDRESS = 'stun:stun.xten.com';
