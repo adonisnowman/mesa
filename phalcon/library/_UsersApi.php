6135b61cdbc10aa5f770f5c3dee5e909cd2caf20
adonis adonis <adonisnowman@gmail.com>
2022-10-05 16:37:33 +0800

    add Admin.UserImages

--- phalcon/library/_UsersApi.php
+++ phalcon/library/_UsersApi.php
@@ -16,7 +16,7 @@ class _UsersApi
 
 
                 $Item['videos'] = Tools::getFileList("/home/cfd888/public_html/swoole.bestaup.com/video/".$Item['UniqueID'],"[a-zA-Z0-9]+\.gif");
-                $Item['Images'] = Tools::getFileList("/home/cfd888/public_html/swoole.bestaup.com/ImagesDir/".$Item['UniqueID'],"[a-zA-Z0-9]+\.gif");
+                $Item['Images'] = Tools::getFileList("/home/cfd888/public_html/swoole.bestaup.com/ImagesDir/".$Item['UniqueID'],"[a-zA-Z0-9]+\.jpg");
                 if(!empty($Item['videos']))
                 foreach($Item['videos'] AS &$video){
                     $video = $Item['UniqueID']."/".$video;
