ac14f73c3a9175ca7ef1fd51a455150d35de725c
adonis adonis <adonisnowman@gmail.com>
2022-11-09 21:23:36 +0800

    $_SERVER['SERVER_ADDR']

--- swoole_ssl.php
+++ swoole_ssl.php
@@ -38,7 +38,7 @@ $ws = new swoole_websocket_server("0.0.0.0", 8080, SWOOLE_PROCESS, SWOOLE_SOCK_T
 $Response = new Response();
 $fullchain = "/etc/letsencrypt/live/swoole.bestaup.com/fullchain.pem";
 $privkey = "/etc/letsencrypt/live/swoole.bestaup.com/privkey.pem";
-
+if (file_exists($fullchain)) $BestaupDefault = true;
 if (file_exists("/etc/letsencrypt/live/adonis.tw-0002/fullchain.pem")) $fullchain = "/etc/letsencrypt/live/adonis.tw-0002/fullchain.pem";
 if (file_exists("/etc/letsencrypt/live/adonis.tw-0002/privkey.pem")) $privkey = "/etc/letsencrypt/live/adonis.tw-0002/privkey.pem";
 
@@ -58,7 +58,7 @@ $SwooleSetting = [
     'chroot' => '/data/server/',
     'open_cpu_affinity' => true,
     'cpu_affinity_ignore' => [0, 1],
-    'pid_file' => __DIR__.'/server.pid',
+    'pid_file' => __DIR__ . '/server.pid',
 
     // Server
     'reactor_num' => 8,
@@ -96,14 +96,14 @@ $SwooleSetting = [
 
     // TCP
     'input_buffer_size' => 2097152,
-    'buffer_output_size' => 32*1024*1024, // byte in unit
+    'buffer_output_size' => 32 * 1024 * 1024, // byte in unit
     'tcp_fastopen' => false,
     'max_conn' => 1000,
     'tcp_defer_accept' => 5,
     'open_tcp_keepalive' => true,
     'open_tcp_nodelay' => false,
     // 'pipe_buffer_size' => 32 * 1024*1024,
-    'socket_buffer_size' => 128 * 1024*1024,
+    'socket_buffer_size' => 128 * 1024 * 1024,
 
     // Kernel
     'backlog' => 512,
@@ -212,22 +212,21 @@ $SwooleSetting = [
 
 ];
 
-var_dump($_SERVER);
-exit;
-if($_SERVER['SERVER_NAME']) $SwooleUrl = "https://adonis.bestaup.com/Swoole";
-    else $SwooleUrl = "http://mesa.adonis.tw/Swoole";
+
+if ($BestaupDefault) $SwooleUrl = "https://adonis.bestaup.com/Swoole";
+else $SwooleUrl = "http://mesa.adonis.tw/Swoole";
 
 
 //環境參數設定
 $SwooleSetting = [];
-$SwooleSetting['daemonize'] = false; 
+$SwooleSetting['daemonize'] = false;
 $SwooleSetting['ssl_cert_file'] = $fullchain;
 $SwooleSetting['ssl_key_file'] = $privkey;
 //配置参数
 $ws->set($SwooleSetting);
 
 //监听WebSocket连接打开事件
-$ws->on('open', function ($ws, $request) use ($connections , $SwooleUrl) {
+$ws->on('open', function ($ws, $request) use ($connections, $SwooleUrl) {
 
 
     if ((int)  round(((float)microtime(true))) == $request->server['master_time'])
@@ -239,7 +238,7 @@ $ws->on('open', function ($ws, $request) use ($connections , $SwooleUrl) {
     $user_md5 = md5($request->header['user-agent'] . $request->server['remote_addr']);
     echo "client-{$request->fd} is open [{$user_md5}] \n";
 
-    $url = $SwooleUrl;
+    $SwooleUrl = $SwooleUrl;
 
 
     $Data['shortUniqueID'] = $shortUniqueID;
@@ -248,11 +247,11 @@ $ws->on('open', function ($ws, $request) use ($connections , $SwooleUrl) {
     $Data['HTTP_HOST'] = $request->header['origin'];
     $Data['REMOTE_ADDR'] = $request->server['remote_addr'];
     $Data['HTTP_USER_AGENT'] = $request->header['user-agent'];
-  
+
     $post['Action'] = "Connect";
     $post['Data'] = json_encode($Data);
 
-    $Return = Tools::httpPost($url, $post, true);
+    $Return = Tools::httpPost($SwooleUrl, $post, true);
 
     if (!empty($Return['ErrorMsg'])) {
         $Action = [];
@@ -264,7 +263,7 @@ $ws->on('open', function ($ws, $request) use ($connections , $SwooleUrl) {
         return false;
     }
 
-    if (!empty($Return['Action']) &&  !empty($Return[$Return['Action']])) 
+    if (!empty($Return['Action']) &&  !empty($Return[$Return['Action']]))
         $Return = Tools::fix_element_Key($Return[$post['Action']], ["UniqueID", "shortUniqueID", "UniqueID_UsersToken", "UniqueID_UsersLoginLogs", "user_md5"]);
     else  return $ws->disconnect($request->fd, 1003, 'Pleace Login first');
 
@@ -277,28 +276,28 @@ $ws->on('open', function ($ws, $request) use ($connections , $SwooleUrl) {
 
 
 //监听WebSocket消息事件
-$ws->on('message', function ($ws, $frame) use ($messages, $connections , $SwooleUrl) {
+$ws->on('message', function ($ws, $frame) use ($messages, $connections, $SwooleUrl) {
 
     // frame data comes in as a string
     $output = json_decode($frame->data, true);
     echo "Message: \n";
 
     $client = $connections->get($frame->fd);
-    var_dump($client,$output);
+    var_dump($client, $output);
     if ($output['user_md5'] == $client['user_md5']  && $output['UniqueID'] == $client['UniqueID_UsersLoginLogs']) {
 
         if (!empty($output['Action']) && $output['shortUniqueID'] == $client['shortUniqueID']) {
 
             switch ($output['Action']) {
                 case 'MessageToUsers':
-                    $MessageUsers = Tools::fix_array_Key( $output['MessageUsers'] , "shortUniqueID" );
-                 
+                    $MessageUsers = Tools::fix_array_Key($output['MessageUsers'], "shortUniqueID");
+
                     foreach ($connections as $client) {
                         $Action = [];
                         $Action['MessageTokenUniqueID'] = $output['MessageTokenUniqueID'];
                         $Action['Type'] = "Toast";
                         $Action['Toast'][] = "你有新訊息:" . $output['MessageTokenUniqueID'];
-                        if( in_array( $client['shortUniqueID'] , $MessageUsers ) )
+                        if (in_array($client['shortUniqueID'], $MessageUsers))
                             SendAction($ws, $client['client'], $Action);
                     }
 
@@ -316,14 +315,14 @@ $ws->on('message', function ($ws, $frame) use ($messages, $connections , $Swoole
             return false;
         }
 
-        $url = "http://mesa.adonis.tw/Swoole";
+        
         $Data['shortUniqueID'] = $client['shortUniqueID'];
-       
+
         $post['Action'] = "MessageToken";
         $post['Data'] = json_encode($Data);
 
-        $Return = Tools::httpPost($url, $post , true);
-       
+        $Return = Tools::httpPost($SwooleUrl, $post, true);
+
         if (!empty($Return['ErrorMsg'])) {
             $Action = [];
             $Action['Type'] = "Toast";
@@ -332,7 +331,7 @@ $ws->on('message', function ($ws, $frame) use ($messages, $connections , $Swoole
         }
 
         if (!empty($Return['MessageToken'])) $MessageToken = $Return['MessageToken'];
-       
+
         if (!empty($output['account'])) {
             //首次連線訊息
             $Action = [];
@@ -346,14 +345,13 @@ $ws->on('message', function ($ws, $frame) use ($messages, $connections , $Swoole
             $Action = [];
             $Action['Type'] = "Connections";
             foreach ($connections as $client) {
-                $Action["Connections"][] = ["shortUniqueID" => $client["shortUniqueID"] ];
+                $Action["Connections"][] = ["shortUniqueID" => $client["shortUniqueID"]];
             }
 
             foreach ($connections as $client) {
 
                 SendAction($ws, $client['client'], $Action);
             }
-
         } else if (!empty($output['MessageType']) && !empty($MessageToken[$output['MessageType']]) && $output['shortUniqueID'] == $client['shortUniqueID']) {
 
 
@@ -373,9 +371,6 @@ $ws->on('message', function ($ws, $frame) use ($messages, $connections , $Swoole
             $Action['Toast'][] = "您暫時無法使用此類型的訊息發送功能";
             SendAction($ws, $frame->fd, $Action);
         }
-
-
-        
     } else {
 
         $Action = [];
@@ -386,21 +381,21 @@ $ws->on('message', function ($ws, $frame) use ($messages, $connections , $Swoole
 });
 
 //监听WebSocket连接关闭事件
-$ws->on('close', function ($ws, $fd) use ($connections , $SwooleUrl) {
+$ws->on('close', function ($ws, $fd) use ($connections, $SwooleUrl) {
     echo "client-{$fd} is closed\n";
     $DisClient = $connections->get($fd);
 
 
-    $url = "http://mesa.adonis.tw/Swoole";
+    
 
     $Data = $DisClient;
     unset($Data['client']);
-   
+
     $post['Action'] = "DisConnect";
     $post['Data'] = json_encode($Data);
 
-    $Return = Tools::httpPost($url, $post);
-   
+    $Return = Tools::httpPost($SwooleUrl, $post);
+
     foreach ($connections as $client) {
 
         if ((int) $client['client'] == $fd) continue;
@@ -416,7 +411,7 @@ $ws->on('close', function ($ws, $fd) use ($connections , $SwooleUrl) {
     $Action = [];
     $Action['Type'] = "Connections";
     foreach ($connections as $client) {
-        $Action["Connections"][] = ["shortUniqueID" => $client["shortUniqueID"] ];
+        $Action["Connections"][] = ["shortUniqueID" => $client["shortUniqueID"]];
     }
 
     foreach ($connections as $client) {
