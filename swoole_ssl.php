b130a16012a7377936a4ffbba6bf10b518871b54
adonis adonis <adonisnowman@gmail.com>
2022-11-09 21:02:00 +0800

    SwooleSetting

--- swoole_ssl.php
+++ swoole_ssl.php
@@ -34,7 +34,7 @@ $connections->column('user_md5', Table::TYPE_STRING, 750);
 
 $connections->create();
 //创建websocket服务器对象，监听0.0.0.0:9501端口，开启SSL隧道
-$ws = new swoole_websocket_server("0.0.0.0", 9509, SWOOLE_PROCESS, SWOOLE_SOCK_TCP | SWOOLE_SSL);
+$ws = new swoole_websocket_server("0.0.0.0", 9501, SWOOLE_PROCESS, SWOOLE_SOCK_TCP | SWOOLE_SSL);
 $Response = new Response();
 $fullchain = "/etc/letsencrypt/live/swoole.bestaup.com/fullchain.pem";
 $privkey = "/etc/letsencrypt/live/swoole.bestaup.com/privkey.pem";
